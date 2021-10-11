24e51dcd8744aa51a67e23f4fbbd3195d495df09
podit <podit@live.co.uk>
2021-10-11 20:47:45 +0100

    modified to 80 char line limit, removed comments from definition file

--- blocks.h
+++ blocks.h
@@ -1,10 +1,11 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   // filesystem usage info
-  {" ~: ", "lsblk -o label,fsavail,size | awk '/^hom/ { print $2\"/\"$3 }'", 30, 0},
-  {" /: ", "lsblk -o label,fsavail,size | awk '/^rut/ { print $2\"/\"$3 }'", 30, 0},
+  {" ~: ", "lsblk -o label,fsavail,size | awk '/^hom/ { print $2\"/\"$3 }'",
+    30, 0},
+  {" /: ", "lsblk -o label,fsavail,size | awk '/^rut/ { print $2\"/\"$3 }'",
+    30, 0},
   
   // memory usage info
 	{" swp: ", "free -h --si | awk '/^Swap/ { print $3\"/\"$2 }'",	30,		0},
@@ -16,11 +17,13 @@ static const Block blocks[] = {
   //{" cpu: ", "uptime | awk '{ print
 
   // battery info
-  {" bat: ", "acpi -b | awk '/^Battery 0:/ {print $4\" \"$3\" \"$5 }' | sed s/:[0-9][0-9]$//g | sed s/,//g | sed s/Discharging/-/g | sed s/Charging/+/g | sed s/Unknown/u/g | sed s/Full/f/g | sed s/f\" \"/f/g", 30, 0},
+  {" bat: ", "acpi -b | awk '/^Battery 0:/ {print $4\" \"$3\" \"$5 }' |\
+    sed s/:[0-9][0-9]$//g | sed s/,//g | sed s/Discharging/-/g |\
+    sed s/Charging/+/g | sed s/Unknown/u/g | sed s/Full/f/g |\
+    sed s/f\" \"/f/g", 30, 0},
 
 	{" ", "date '+%b %d (%a) %H:%M'",					5,		0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " |";
 static unsigned int delimLen = 5;
